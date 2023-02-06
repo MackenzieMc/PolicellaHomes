8281d91f42f22b1cc3dd4a8edee5bc2914132411
mackenzie <macmcclemont@hotmail.com>
2023-02-06 18:57:43 -0500

    making changes

--- pages/ModelHomes/[ModelHomeID].js
+++ pages/ModelHomes/[ModelHomeID].js
@@ -1,38 +1,38 @@
-import { modelData } from '../../public/data/model_homes_data.json'
-import fsPromises from 'fs/promises';
-import path from 'path';
+// import { modelData } from '../../public/data/model_homes_data.json'
+// import fsPromises from 'fs/promises';
+// import path from 'path';
 
-export const getStaticPaths = async () => {
+// export const getStaticPaths = async () => {
 
-    const filePath = path.join(process.cwd(), '/public/data/model_homes_data.json');
-    const jsonData = await fsPromises.readFile(filePath);
-    const objectData = JSON.parse(jsonData);
+//     const filePath = path.join(process.cwd(), '/public/data/model_homes_data.json');
+//     const jsonData = await fsPromises.readFile(filePath);
+//     const objectData = JSON.parse(jsonData);
 
-    const modelHouse = objectData.modelHomes
+//     const modelHouse = objectData.modelHomes
 
-    const res = (await import ('../../public/data/model_homes_data.json')).default
+//     const res = (await import ('../../public/data/model_homes_data.json')).default
     
-    const paths = res.map( data => {
-        return {
-            params: { id: data.id.toString()}
-        }
-    })
-    return {
-        paths,
-        fallback: false
-    }
-}
-function ModelHomeDetail () {
+//     const paths = res.map( data => {
+//         return {
+//             params: { id: data.id.toString()}
+//         }
+//     })
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
+// function ModelHomeDetail () {
     
-    return (
-        <section>
-            <div className="wrapper">
-                <div className="modelNameTextContainer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                </div>
-            </div>
-        </section>
-    )
-}
+//     return (
+//         <section>
+//             <div className="wrapper">
+//                 <div className="modelNameTextContainer">
+//                     <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+//                 </div>
+//             </div>
+//         </section>
+//     )
+// }
 
-export default ModelHomeDetail
+// export default ModelHomeDetail
