eb57e6280164ab0510b5fe655c1c3d85a6ce0a2a
mackenzie <macmcclemont@hotmail.com>
2023-08-31 02:11:38 -0400

    tb

--- components/Preamble.js
+++ components/Preamble.js
@@ -2,8 +2,8 @@ import Link from "next/link"
 import Image from "next/image"
 import pr1 from '../public/images/Preamble1.jpg'
 import pr2 from '../public/images/Preamble2.jpg'
-import pr3 from '../public/images/Preamble3.jpg'
-import pr4 from '../public/images/Preamble4.jpg'
+import pr3 from '../public/images/divOne.jpg'
+import pr4 from '../public/images/aboutOne.jpg'
 
 
 function Preamble () {
